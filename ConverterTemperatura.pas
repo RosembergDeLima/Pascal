916cdb0b8b1d3881b7b9ac2ea341f79dd9f40550
Rosemberg de Lima <rosembergdlt@gmail.com>
2016-06-16 20:51:24 -0300

    ConverterTemperatura adicionado ao repositório

---
+++ ConverterTemperatura.pas
@@ -0,0 +1,12 @@
+Program ConverterTemperatura;
+Var
+  F, C: Real;
+  
+Begin
+  write('Fahrenhite: ');
+	read(F);
+	
+	C:=(5/9)*(F-32);
+	
+	write('Celsius: ', C:5:2);  
+End.                                
