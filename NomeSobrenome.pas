10ccca1619a086371433464109c52b1c13af2465
Rosemberg de Lima <rosembergdlt@gmail.com>
2016-06-16 21:40:10 -0300

    NomeSobrenome adicionado ao repositório

---
+++ NomeSobrenome.pas
@@ -0,0 +1,11 @@
+Program NomeSobrenome;
+Var
+  nome, sobrenome: String;
+Begin
+  write('Digite o nome: ');
+	read(nome);
+	write('Digite o sobrenome: ');
+	read(sobrenome);
+	
+	write(sobrenome, ', ', nome);  
+End.
