9549a936f78555b08cde89acd029f1d2ffb2bdfc
Rosemberg de Lima <rosembergdlt@gmail.com>
2016-06-18 22:14:33 -0300

    Maior adicionado ao repositório

---
+++ Maior.pas
@@ -0,0 +1,12 @@
+Program Maior;
+Var
+	a, b: Integer;
+Begin
+  write('Digite o primeiro número: ');
+  read(a);
+  write('Digite o segundo número: ');
+  read(b);
+  
+  if a>b then
+  	write(a);
+End.
